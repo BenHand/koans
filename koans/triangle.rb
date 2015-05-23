d92a71f4def6229510a9cb29714c60c44cb8ff1d
Benjamin Hand <gneeshot@gmail.com>
2015-05-23 11:13:47 -0500

    finished up until 87 on sandwiches

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+
+  a, b, c = [a, b, c].sort
+  raise TriangleError if a <= 0 || a + b <= c
+  return :equilateral if a == c
+  return :isosceles if a == b || b == c
+  return :scalene
+
 end
 
 # Error class used in part 2.  No need to change this code.
